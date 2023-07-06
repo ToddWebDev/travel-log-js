8342fe395ae708e2c5cef8e029f81b0087ea0862
ToddWebDev <webdevtodd@gmail.com>
2023-07-06 13:38:14 -0600

    Add map marker filter

--- main.js
+++ main.js
@@ -2823,6 +2823,8 @@ map.on('load', () => {
   addMarker(origin, '#fcba03')
 
   viewportWidth = window.innerWidth
+
+  addLocationFilter()
 })
 
 function addHomeListing() {
@@ -2853,6 +2855,67 @@ function addHomeListing() {
     }
     this.parentNode.classList.add('active')
   })
+}
+
+function addLocationFilter() {
+  // Create a new select element
+  const selectElement = document.createElement('select')
+
+  selectElement.className = 'input-location-filter'
+
+  // Create and add option elements
+  const allLocations = document.createElement('option')
+  allLocations.value = 'allLocations'
+  allLocations.textContent = 'All Locations'
+  selectElement.appendChild(allLocations)
+
+  const skiResorts = document.createElement('option')
+  skiResorts.value = 'skiResorts'
+  skiResorts.textContent = 'Ski Resorts'
+  selectElement.appendChild(skiResorts)
+
+  const golfCourses = document.createElement('option')
+  golfCourses.value = 'golfCourses'
+  golfCourses.textContent = 'Golf Courses'
+  selectElement.appendChild(golfCourses)
+
+  const campSites = document.createElement('option')
+  campSites.value = 'campSites'
+  campSites.textContent = 'Camp Sites'
+  selectElement.appendChild(campSites)
+
+  // Add a click event listener to the select element
+  selectElement.addEventListener('change', function (event) {
+    const selectedValue = selectElement.value
+
+    switch (selectedValue) {
+      case 'allLocations':
+        removeAllHTMLMarkers()
+        addHTMLMarkers(golf_courses.features)
+        addHTMLMarkers(camp_sites.features)
+        addHTMLMarkers(ski_resorts.features)
+        break
+      case 'golfCourses':
+        removeAllHTMLMarkers()
+        addHTMLMarkers(golf_courses.features)
+        break
+      case 'skiResorts':
+        removeAllHTMLMarkers()
+        addHTMLMarkers(ski_resorts.features)
+        break
+      case 'campSites':
+        removeAllHTMLMarkers()
+        addHTMLMarkers(camp_sites.features)
+        break
+      default:
+        removeAllHTMLMarkers()
+        break
+    }
+  })
+
+  // Append the select element to the page
+  const container = document.getElementById('map')
+  container.appendChild(selectElement)
 }
 
 function buildLogsList(year) {
@@ -2924,13 +2987,13 @@ function buildAllLogs() {
   })
 }
 
-function addHTMLMarkers(features, color) {
+function addHTMLMarkers(features) {
   /* For each feature in the GeoJSON object above: */
-  for (const feature of features) {
+  for (const [index, feature] of features.entries()) {
     /* Create a div element for the marker. */
     const el = document.createElement('div')
     /* Assign a unique `id` to the marker. */
-    el.id = `marker-${feature.properties.id}`
+    el.id = `marker-${feature.properties.type}-${feature.properties.mode}-${index}`
     /* Assign the `marker` class to each marker for styling. */
     el.className =
       feature.properties.type === 'golf-course'
@@ -2981,6 +3044,26 @@ function addHTMLMarkers(features, color) {
       }
     })
   }
+}
+
+function removeHTMLMarkers(features) {
+  /* For each feature in the GeoJSON object above: */
+  for (const [index, feature] of features.entries()) {
+    // Get the element by its ID
+    const element = document.getElementById(`marker-${feature.properties.type}-${feature.properties.mode}-${index}`)
+
+    // Check if the element exists
+    if (element) {
+      // Remove the element from the DOM
+      element.remove()
+    }
+  }
+}
+
+function removeAllHTMLMarkers() {
+  removeHTMLMarkers(golf_courses.features)
+  removeHTMLMarkers(camp_sites.features)
+  removeHTMLMarkers(ski_resorts.features)
 }
 
 function addMarker(coordinates, color) {
